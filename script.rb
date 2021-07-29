35b057fa1af4340803e029c79fba583a4a91029b
Methal Abeywickrama <methal8779@gmail.com>
2021-07-29 13:21:47 +0530

    all? works

--- script.rb
+++ script.rb
@@ -28,10 +28,24 @@ module Enumerable
     end
     arr
   end
+
+  def my_all?
+    i = 0
+    arr = []
+    while i < length
+      state = yield self[i]
+      arr.push(state) if state == true
+      i += 1
+    end
+    return false if arr.length != self.length
+
+    true
+  end
 end
 
 puts 'my_each vs. each'
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+other_numbers = [2, 4, 6, 8]
 puts 'my_each'
 numbers.my_each { |item| puts item * 5 }
 puts 'each'
@@ -44,3 +58,11 @@ puts 'my_select'
 puts (numbers.my_select { |i| i.even?})
 puts 'select'
 puts (numbers.select { |i| i.even?})
+puts 'my_all'
+puts (numbers.my_all? { |i| i.even?})
+puts 'all'
+puts (numbers.all? { |i| i.even?})
+puts 'my_all'
+puts (other_numbers.my_all? { |i| i.even?})
+puts 'all'
+puts (other_numbers.all? { |i| i.even?})
